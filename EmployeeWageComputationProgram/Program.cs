a22256e1377a7b35e561955121ef355f2a7edd4f
PREETI2096 <preetirg96@gmail.com>
2023-05-29 19:30:07 +0530

    Added : Attendance program

---
+++ EmployeeWageComputationProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputationProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeWage emp = new EmployeeWage();
+            emp.EmployeeCheckAttendance();
+        }
+    }
+}
